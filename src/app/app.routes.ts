5fd71cc8e8790e516b6daab89ee3ec539e9f6e52
getulio.souza <getulio.souza@evolutix.com.br>
2024-11-24 21:32:04 -0300

    bugfix: import-with-error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { ModuleWithProviders } from '@angular/core';
 
-export const routes: Routes = [];
+export const APP_ROUTES: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  {path: 'cursos', component: CursosComponent}
+];
+
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(APP_ROUTES);
