b638d87b34e07f6891407ed8240c4e5953848b04
Getulio Souza <getuliosouza8@gmail.com>
2024-11-27 16:23:05 -0300

    feature: ng-on-init-and-on-destroy

--- src/app/components/cursos/curso-detalhe/curso-detalhe.component.ts
+++ src/app/components/cursos/curso-detalhe/curso-detalhe.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -7,13 +8,24 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './curso-detalhe.component.html',
   styleUrl: './curso-detalhe.component.scss'
 })
-export class CursoDetalheComponent {
+export class CursoDetalheComponent implements OnInit {
 
   id: string;
+  inscricao!: Subscription;
 
   constructor(private route: ActivatedRoute){
     // console.log(this.route)
     this.id = this.route.snapshot.params['id']
   }
-  idCurso!: string;
+
+  ngOnInit(): void {
+    //params representa um tipo de objeto
+    this.inscricao = this.route.params.subscribe((params: any)=> {
+      this.id = params['id'];
+    })
+  }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe()
+  }
 }
