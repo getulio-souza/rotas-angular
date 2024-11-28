18686b0b240aa71dfdf0a5b98f00aecd1735e75f
Getulio Souza <getuliosouza8@gmail.com>
2024-11-28 16:09:54 -0300

    feature: creating-an-object-to-be-shown

--- src/app/components/cursos/curso-detalhe/curso-detalhe.component.ts
+++ src/app/components/cursos/curso-detalhe/curso-detalhe.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { CursosService } from '../services/cursos.service';
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -10,10 +11,11 @@ import { Subscription } from 'rxjs';
 })
 export class CursoDetalheComponent implements OnInit {
 
-  id: string;
+  id!: number;
   inscricao!: Subscription;
+  curso!:any;
 
-  constructor(private route: ActivatedRoute){
+  constructor(private route: ActivatedRoute, private cursosService: CursosService){
     // console.log(this.route)
     this.id = this.route.snapshot.params['id']
   }
@@ -22,10 +24,14 @@ export class CursoDetalheComponent implements OnInit {
     //params representa um tipo de objeto
     this.inscricao = this.route.params.subscribe((params: any)=> {
       this.id = params['id'];
+
+      this.curso = this.cursosService.getCursoById(this.id);
     })
   }
 
   ngOnDestroy(){
-    this.inscricao.unsubscribe()
+    if(this.inscricao){
+      this.inscricao.unsubscribe();
+    }
   }
 }
--- src/app/components/cursos/cursos.component.ts
+++ src/app/components/cursos/cursos.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CursosService } from './services/cursos.service';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cursos',
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterModule],
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.scss'
 })
-export class CursosComponent {
+export class CursosComponent implements OnInit{
 
+  @Input()cursos!: any[];
+
+  constructor(private cursosService: CursosService){}
+
+  ngOnInit(){
+    this.cursos = this.cursosService.getCursos();
+  }
 }
